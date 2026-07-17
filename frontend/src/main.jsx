a4c5978067e909cee985c345062db9248088ffe1
Lamininto <cloudstechtz@gmail.com>
2026-07-17 17:00:04 +0300

    Fix import statement formatting in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Or whatever your main stylesheet file is named!
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
