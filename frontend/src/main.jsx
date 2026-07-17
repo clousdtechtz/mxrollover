c7e5355441b9b001c271b6ad7fd9efd3381abb65
Lamininto <cloudstechtz@gmail.com>
2026-07-17 16:57:10 +0300

    Add main stylesheet import to main.jsx
    Import main stylesheet to apply styles to the application.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // Or whatever your main stylesheet file is named!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
